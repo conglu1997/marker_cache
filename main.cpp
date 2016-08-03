d6ce74383dc7c9849adf7cf16ab3f853390f781b
Cong Lu <cong.lu@balliol.ox.ac.uk>
2016-08-03 14:09:56 +0100

    using namespace std

--- main.cpp
+++ main.cpp
@@ -11,14 +11,14 @@ using namespace std;
 // Need to delete after using test data.
 vector<char*> generate_test_data(size_t num_elems, size_t width) {
     vector<char*> v;
-    std::string chars(
+    string chars(
         "abcdefghijklmnopqrstuvwxyz"
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         "1234567890"
         "!@#$%^&*()"
         "`~-_=+[{]}\\|;:'\",<.>/? ");
-    std::default_random_engine rng(std::random_device{}());
-    std::uniform_int_distribution<int> distribution(0, chars.size() - 1);
+    default_random_engine rng(random_device{}());
+    uniform_int_distribution<int> distribution(0, chars.size() - 1);
     for (auto i = 0; i < num_elems; ++i) {
         auto s = new char[width];
         for (size_t i = 0; i < width; ++i) s[i] = chars[distribution(rng)];
@@ -36,15 +36,15 @@ int main() {
     marker_cache m(
         bytes_allocated);  // bytes -- should be on the order of 100MB
 
-    auto test_size = 1000000;  // million
+    auto test_size = 1000000;  // 1 million
     auto test_fprate = 0.001;  // 1 in 1 thousand
     auto test_width = 8;       // 8 byte char[]
     auto falsepos = 0;
     static_assert(sizeof(char) == 1, "Chars used are the correct size.");
 
-    std::cout << "Object occupies "
-              << m.create(1, test_fprate, test_size, 0, false, false)
-              << " bytes." << std::endl;
+    cout << "Object occupies "
+         << m.create(1, test_fprate, test_size, 0, false, false) << " bytes."
+         << endl;
 
     cout << "Priming test data:" << endl;
 
@@ -53,20 +53,17 @@ int main() {
 
     cout << "Test data generated." << endl;
 
-    std::chrono::steady_clock::time_point begin =
-        std::chrono::steady_clock::now();
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
     for (auto str : v) m.insert_into(1, str, test_width);
 
-    std::chrono::steady_clock::time_point end =
-        std::chrono::steady_clock::now();
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
 
     cout << "Finished " << test_size << " inserts in "
-         << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin)
-                .count()
+         << chrono::duration_cast<chrono::milliseconds>(end - begin).count()
          << " milliseconds." << endl;
 
-    begin = std::chrono::steady_clock::now();
+    begin = chrono::steady_clock::now();
 
     for (auto str : v) {
         if (m.lookup_from(1, str, test_width) == 0) {
@@ -75,16 +72,15 @@ int main() {
         }
     }
 
-    end = std::chrono::steady_clock::now();
+    end = chrono::steady_clock::now();
 
     cout << "Finished " << test_size << " checks in "
-         << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin)
-                .count()
+         << chrono::duration_cast<chrono::milliseconds>(end - begin).count()
          << " milliseconds." << endl;
 
     for (auto str : v) delete[] str;
 
-    begin = std::chrono::steady_clock::now();
+    begin = chrono::steady_clock::now();
 
     for (auto str : v2) {
         if (m.lookup_from(1, str, 8)) {
@@ -92,11 +88,10 @@ int main() {
         }
     }
 
-    end = std::chrono::steady_clock::now();
+    end = chrono::steady_clock::now();
 
     cout << "Finished " << test_size << " false positive checks in "
-         << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin)
-                .count()
+         << chrono::duration_cast<chrono::milliseconds>(end - begin).count()
          << " milliseconds. Observed fp rate: "
          << (double)falsepos / (double)test_size
          << ", Desired fp rate: " << test_fprate << endl;
