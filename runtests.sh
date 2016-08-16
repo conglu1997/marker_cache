7bc939661f7c561460656f6744b2528d00ad26f8
Cong Lu <cong.lu@balliol.ox.ac.uk>
2016-08-16 11:45:29 +0100

    auto run unit tests

--- runtests.sh
+++ runtests.sh
@@ -1,1 +1,4 @@
+rm UnitTests
 g++ -o UnitTests UnitTests.cpp markercache.cpp shmbloomfilter.cpp hash.cpp -I/export/home/build/V9.4_product_LV4_Patch_CL5_1/sas/code/ced/dbapp -std=c++0x -lrt -pthread
+chmod 777 UnitTests
+./UnitTests
