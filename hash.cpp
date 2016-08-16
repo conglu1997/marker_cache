618f9071ce44f83c5ae38788aa7a704bea04ee61
Cong Lu <cong.lu@balliol.ox.ac.uk>
2016-08-16 14:54:16 +0100

    Don't throw on too large error

--- hash.cpp
+++ hash.cpp
@@ -33,7 +33,7 @@ hash_function::hash_function(size_t seed) : h3_(seed) {}
 
 size_t hash_function::operator()(char *data, int size) const {
     // Current max is 36 bytes - settable in hash.h
-    if (size > max_obj_size) throw std::runtime_error("Object too large!");
+    if (size > max_obj_size) return h3_(data, max_obj_size);
     return size == 0 ? 0 : h3_(data, size);
 }
 
