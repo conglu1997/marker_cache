1eea94c7455c3310f15756d6815e698ef5301aff
Cong Lu <cong.lu@balliol.ox.ac.uk>
2016-08-17 12:24:18 +0100

    Make SD as well

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,6 @@
-rm UnitTests
-g++ -o UnitTests UnitTests.cpp markercache.cpp shmbloomfilter.cpp hash.cpp -I/. -std=c++0x -lrt -pthread
-chmod 777 UnitTests
-./UnitTests
+rm DBAppUnitTests
+g++ -o DBAppUnitTests DBAppUnitTests.cpp markercache.cpp shmbloomfilter.cpp hash.cpp -I/export/home/build/V9.4_product_LV4_Patch_CL5_1/sas/code/ced/dbapp -std=c++0x -lrt -pthread
+chmod 777 DBAppUnitTests
+rm SDUnitTests
+g++ -o SDUnitTests SDUnitTests.cpp markercache.cpp shmbloomfilter.cpp hash.cpp -I/export/home/build/V9.4_product_LV4_Patch_CL5_1/sas/code/ced/dbapp -std=c++0x -lrt -pthread
+chmod 777 SDUnitTests
