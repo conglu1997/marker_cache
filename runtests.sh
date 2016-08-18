a58f45e27f8adf00e584e63660ef17ddcc5fe830
Cong Lu <cong.lu@balliol.ox.ac.uk>
2016-08-18 11:08:13 +0100

    proper includes

--- runtests.sh
+++ runtests.sh
@@ -1,9 +1,9 @@
 rm -f DBAppUnitTests
-g++ -o DBAppUnitTests DBAppUnitTests.cpp markercache.cpp shmbloomfilter.cpp mmh3.cpp -I/export/home/build/V9.4_product_LV4_Patch_CL5_1/sas/code/ced/dbapp -std=c++0x -lrt -pthread
+g++ -o DBAppUnitTests DBAppUnitTests.cpp markercache.cpp shmbloomfilter.cpp mmh3.cpp -I./ -std=c++0x -lrt -pthread
 chmod 777 DBAppUnitTests
 rm -f SDUnitTests
-g++ -o SDUnitTests SDUnitTests.cpp markercache.cpp shmbloomfilter.cpp mmh3.cpp -I/export/home/build/V9.4_product_LV4_Patch_CL5_1/sas/code/ced/dbapp -std=c++0x -lrt -pthread
+g++ -o SDUnitTests SDUnitTests.cpp markercache.cpp shmbloomfilter.cpp mmh3.cpp -I./ -std=c++0x -lrt -pthread
 chmod 777 SDUnitTests
 rm -f TestingSHM
-g++ -o TestingSHM TestingSHM.cpp markercache.cpp shmbloomfilter.cpp mmh3.cpp -I/export/home/build/V9.4_product_LV4_Patch_CL5_1/sas/code/ced/dbapp -std=c++0x -lrt -pthread
+g++ -o TestingSHM TestingSHM.cpp markercache.cpp shmbloomfilter.cpp mmh3.cpp -I./ -std=c++0x -lrt -pthread
 chmod 777 TestingSHM
