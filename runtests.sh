7ec76b5d5e2b781707700de547cc24a1afbbf48e
Cong Lu <cong.lu@balliol.ox.ac.uk>
2016-08-18 10:10:06 +0100

    force removes

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,6 @@
-rm DBAppUnitTests
+rm -f DBAppUnitTests
 g++ -o DBAppUnitTests DBAppUnitTests.cpp markercache.cpp shmbloomfilter.cpp mmh3.cpp -I/export/home/build/V9.4_product_LV4_Patch_CL5_1/sas/code/ced/dbapp -std=c++0x -lrt -pthread
 chmod 777 DBAppUnitTests
-rm SDUnitTests
+rm -f SDUnitTests
 g++ -o SDUnitTests SDUnitTests.cpp markercache.cpp shmbloomfilter.cpp mmh3.cpp -I/export/home/build/V9.4_product_LV4_Patch_CL5_1/sas/code/ced/dbapp -std=c++0x -lrt -pthread
 chmod 777 SDUnitTests
