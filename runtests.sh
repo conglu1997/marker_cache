144f87e9db988069a672eb6ac7dd1f3a6562ce89
Cong Lu <cong.lu@balliol.ox.ac.uk>
2016-08-18 11:06:38 +0100

    include test file

--- runtests.sh
+++ runtests.sh
@@ -4,3 +4,6 @@ chmod 777 DBAppUnitTests
 rm -f SDUnitTests
 g++ -o SDUnitTests SDUnitTests.cpp markercache.cpp shmbloomfilter.cpp mmh3.cpp -I/export/home/build/V9.4_product_LV4_Patch_CL5_1/sas/code/ced/dbapp -std=c++0x -lrt -pthread
 chmod 777 SDUnitTests
+rm -f TestingSHM
+g++ -o TestingSHM TestingSHM.cpp markercache.cpp shmbloomfilter.cpp mmh3.cpp -I/export/home/build/V9.4_product_LV4_Patch_CL5_1/sas/code/ced/dbapp -std=c++0x -lrt -pthread
+chmod 777 TestingSHM
